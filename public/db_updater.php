345306fda7a8b57d7cbe8447baf8792a10672b9a
Henry Tottle <henry@Henrys-MacBook-Air.local>
2025-01-06 21:56:36 +0000

    updated readme and have been noodling around with single use scripts

---
+++ public/db_updater.php
@@ -0,0 +1,35 @@
+<?php
+$db = new PDO('mysql:host=db; dbname=subscriptions_test_database', 'root','password');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+
+$query = $db->prepare('SELECT `isbn` FROM `books`');
+$query->execute();
+$books = $query->fetchAll();
+
+
+//foreach ($books as $book)
+//{
+//    $isbn = $book['isbn'];
+//    $firstEight = substr($isbn, 0, 8);
+//    $image = $firstEight . '/' . $isbn;
+//    $query = $db->prepare('UPDATE `books` SET `image` = :image WHERE isbn = :isbn');
+//    $query->execute(['image' => $image, 'isbn' => $isbn]);
+//
+//}
+
+$fantasyBooks = file_get_contents('fantasyISBNS.txt');
+$fantasyBooksArray = explode(',',$fantasyBooks);
+echo gettype($fantasyBooksArray);
+//var_dump($fantasyBooksArray);
+foreach($fantasyBooksArray as $fantasyBook){
+    $subject = '["FANTASY"]';
+    $fantasyBook = trim($fantasyBook);
+    try {
+        $query = $db->prepare('UPDATE `books` SET `subject` = :subject WHERE `isbn` = :isbn');
+        $query->execute(['subject' => $subject, 'isbn' => $fantasyBook]);
+    } catch(PDOException $e) {
+        echo 'It didn\'t work...' . $e->getMessage();
+    }
+}
