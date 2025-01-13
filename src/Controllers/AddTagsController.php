5e1d2f3f5123ab454c6d17e72bd9ea8ff8876fa2
Henry Tottle <henry@Henrys-MacBook-Air.local>
2025-01-13 12:36:51 +0000

    running into preflight issues with FE

--- src/Controllers/AddTagsController.php
+++ src/Controllers/AddTagsController.php
@@ -17,15 +17,31 @@ class AddTagsController
 
     public function __invoke(RequestInterface $request, ResponseInterface $response): ResponseInterface
     {
+        if ($request->getMethod() === 'OPTIONS') {
+            return $response
+                ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173') // Allow all origins (you can restrict it to specific domains)
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+                ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+                ->withHeader('Access-Control-Allow-Credentials', 'true')
+                ->withStatus(200);
+        }
+
         $data = $request->getParsedBody();
         $tag = $data['tag'];
         $id = $data['book_id'];
+
+        $this->model->addTag($id, $tag);
+
         $responseBody = [
             'message' => 'Tag successfully added',
             'status' => 201,
             'tag' => $tag
         ];
         $this->model->addTag($id ,$tag);
-        return $response->withHeader('Access-Control-Allow-Origin', '*')->withJson($responseBody);
+        return $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+                        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+                        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+                        ->withHeader('Access-Control-Allow-Credentials', 'true')
+                        ->withJson($responseBody);
     }
 }
